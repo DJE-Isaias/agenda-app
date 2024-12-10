e3f7ed4a7ce3f4c2dd0a0b66e73cf8e69084d49b
Isaias Bernabe <isaias.bernabe@ab-inbev.com>
2024-12-09 22:07:35 -0400

    chore: dynamic route2

--- src/app/api/contacts/[id]/route.ts
+++ src/app/api/contacts/[id]/route.ts
@@ -2,28 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { getUserFromCookies } from "@/lib/jwt";
 
-// export async function GET(
-//   req: NextRequest,
-//   { params }: { params: Promise<{ id: string }> }
-// ) {
-//   const id = (await params).id;
-//   const user = await getUserFromCookies();
-
-//   if (!user) {
-//     return NextResponse.json({ error: "No autorizado" }, { status: 401 });
-//   }
-
-//   const contact = await prisma.contact.findUnique({
-//     where: { id },
-//   });
-
-//   if (!contact || contact.userId !== user.id) {
-//     return NextResponse.json({ error: "No encontrado" }, { status: 404 });
-//   }
-
-//   return NextResponse.json(contact);
-// }
-
 export async function PUT(
   req: NextRequest,
   { params }: { params: Promise<{ id: string }> }
